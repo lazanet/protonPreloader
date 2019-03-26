0b6d4f57401e3e74c4db22b40904ee59657fe62b
lazanet <lazanet96@gmail.com>
2019-03-26 14:13:26 +0100

    Initial upload

---
+++ config.bat
@@ -0,0 +1,15 @@
+@echo off
+cd /D "%~dp0"
+:: -------------------------------------------
+:: Main script part
+:: -------------------------------------------
+
+:: Change next line to call another utility
+start "Utility to start before game" "fw\FlawlessWidescreen.exe"
+
+echo "Arguments to be passed to game executable are: %*"
+
+pause
+
+:: Change next line to change path to game executable
+start "Main game executable" "game.exe %*"
---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char *argv[])
+{
+    std::string s(argv[0]);
+    std::string path = "config.bat";
+    std::size_t pos = s.rfind('\\');
+
+    if (pos != std::string::npos)
+        path = '"' + s.substr(0,pos+1) + "config.bat" + '"';
+
+    //std::cout<<path<<std::endl;
+
+    for (int i=1; i<argc; i++)
+        path+=" "+std::string(argv[i]);
+
+    std::string command = "start \"Launcher\" cmd /c "+path;
+    system(command.c_str());
+    return 0;
+}
